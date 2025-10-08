cafea7b5778cd2ed068346efe4826e0acd301b4d
Administrator <Administrator@A302-7-03>
2025-10-08 12:07:51 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace D11416129
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string arrivalTime = Console.ReadLine();
+            int r1 = Convert.ToInt32(arrivalTime);
+
+            string delayedTime = Console.ReadLine();
+            int r2 = Convert.ToInt32(delayedTime);
+
+            Console.WriteLine(finaltime(r1, r2));
+
+
+
+
+            int finaltime(int arrivalTime, int delayedTime)
+            {
+                //1.輸入
+                //int arrivalTime
+                //int delayedTime
+
+                //2.處理
+                int comput = arrivalTime + delayedTime;
+                comput = comput % 24;
+                //3.輸出
+                int result = comput;
+                return (result);
+            }
+        }
+    }
+}
+
